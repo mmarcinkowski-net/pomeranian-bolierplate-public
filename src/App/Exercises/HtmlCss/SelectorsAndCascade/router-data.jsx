2a778b136041763b76e1a3d88e03fae27d5faa9b
mmarcinkowski-net <mmarcinkowski.net@gmail.com>
2023-06-14 20:05:45 +0200

    CSS - block 10

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,10 @@
+import { SelectorsAndCascade } from './SelectorsAndCascade';
+
+export const SelectorsAndCascadeMetaData = {
+  path: 'selectors-and-cascade',
+  date: '12-12-2023',
+  linkLabel: 'SelectorsAndCascade - 2023-06-14',
+  blockNo: 10,
+  element: <SelectorsAndCascade />,
+  tags: ['selectors', 'class', 'attribute'],
+};
